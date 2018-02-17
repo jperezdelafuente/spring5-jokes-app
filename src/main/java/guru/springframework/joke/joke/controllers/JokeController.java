688342a8962c3e4bbc1dfae0009eea1f575bf713
jperezdelafuente <jesusperez82@gmail.com>
2018-02-17 12:52:32 +0100

    Adding Joke Controller

---
+++ src/main/java/guru/springframework/joke/joke/controllers/JokeController.java
@@ -0,0 +1,23 @@
+package guru.springframework.joke.joke.controllers;
+
+import guru.springframework.joke.joke.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model){
+        model.addAttribute("joke", jokeService.getjoke());
+
+        return "chucknorris";
+    }
+}
